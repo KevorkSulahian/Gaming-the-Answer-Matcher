fd4f1ca7da36f9694f15f0086a8230ddeeefc439
Manas Khatore <manaskhatore@gmail.com>
2025-09-24 00:55:00 +0000

    renamed and reorganized mmlu data

--- datasets/.ipynb_checkpoints/data_sampler-checkpoint.py
+++ datasets/.ipynb_checkpoints/data_sampler-checkpoint.py
@@ -4,11 +4,11 @@ def main():
     # mmlu_pro_qual = pd.read_csv("datasets/mmlu/mmlu_pro_qualitative_test.csv")
     # mmlu_pro_quant = pd.read_csv("datasets/mmlu/mmlu_pro_quantitative_test.csv")
 
-    mmlu_pro_qual_sample = pd.read_csv("datasets/mmlu/mmlu_pro_qualitative_test_sample_large.csv")
-    mmlu_pro_quant_sample = pd.read_csv("datasets/mmlu/mmlu_pro_quantitative_test_sample_large.csv")
+    mmlu_pro_qual_sample_filtered = pd.read_csv("datasets/mmlu/mmlu_pro_qualitative_final_sample.csv")
+    mmlu_pro_quant_sample_filtered = pd.read_csv("datasets/mmlu/mmlu_pro_quantitative_final_sample.csv")
 
-    mmlu_pro_qual_answers = pd.read_csv("answer-generation/mmlu/mmlu-verbose/gpt_mmlu_pro_verbose_qual_test_answers.csv")
-    mmlu_pro_quant_answers = pd.read_csv("answer-generation/mmlu/mmlu-verbose/gpt_mmlu_pro_verbose_quant_test_answers.csv")
+    mmlu_pro_qual_answers = pd.read_csv("answer-matching/scores/mmlu/gpt_mmlu_pro_forward_qual_Qwen3-4B_judge.csv")
+    # mmlu_pro_quant_answers = pd.read_csv("answer-generation/mmlu/mmlu-verbose/gpt_mmlu_pro_verbose_quant_test_answers.csv")
 
     # Define patterns that indicate dependence on answer choices
     patterns = [
@@ -27,34 +27,34 @@ def main():
     ]
 
     # Build regex from patterns
-    regex = "|".join(patterns)
+    # regex = "|".join(patterns)
 
-    # Filter out dependent questions
-    mmlu_pro_qual_sample_filtered = mmlu_pro_qual_sample[
-        ~mmlu_pro_qual_sample["question"].str.lower().str.contains(regex, na=False)
-    ]
-    mmlu_pro_quant_sample_filtered = mmlu_pro_quant_sample[
-        ~mmlu_pro_quant_sample["question"].str.lower().str.contains(regex, na=False)
-    ]
+    # # Filter out dependent questions
+    # mmlu_pro_qual_sample_filtered = mmlu_pro_qual_sample[
+    #     ~mmlu_pro_qual_sample["question"].str.lower().str.contains(regex, na=False)
+    # ]
+    # mmlu_pro_quant_sample_filtered = mmlu_pro_quant_sample[
+    #     ~mmlu_pro_quant_sample["question"].str.lower().str.contains(regex, na=False)
+    # ]
 
     sample_qual_questions = mmlu_pro_qual_sample_filtered["question"].tolist()
     sample_quant_questions = mmlu_pro_quant_sample_filtered["question"].tolist()
 
     answers_sample_qual = mmlu_pro_qual_answers[mmlu_pro_qual_answers['question'].isin(sample_qual_questions)]
-    answers_sample_quant = mmlu_pro_quant_answers[mmlu_pro_quant_answers['question'].isin(sample_quant_questions)]
+    # answers_sample_quant = mmlu_pro_quant_answers[mmlu_pro_quant_answers['question'].isin(sample_quant_questions)]
 
     # Save filtered answers
-    # answers_sample_qual.to_csv(
-    #     "answer-generation/mmlu/mmlu-verbose/gpt_mmlu_pro_verbose_qual_test_answers_sample_large_filtered.csv",
-    #     index=False
-    # )
+    answers_sample_qual.to_csv(
+        "answer-matching/scores/mmlu/forward_qual/gpt_mmlu_pro_forward_qual_Qwen3-4B_judge.csv.csv",
+        index=False
+    )
     # answers_sample_quant.to_csv(
     #     "answer-generation/mmlu/mmlu-verbose/gpt_mmlu_pro_verbose_quant_test_answers_sample_large_filtered.csv",
     #     index=False
     # )
 
-    mmlu_pro_qual_sample_filtered.to_csv("mmlu_pro_qual_filtered.csv")
-    mmlu_pro_quant_sample_filtered.to_csv("mmlu_pro_quant_filtered.csv")
+    # mmlu_pro_qual_sample_filtered.to_csv("mmlu_pro_qual_filtered.csv")
+    # mmlu_pro_quant_sample_filtered.to_csv("mmlu_pro_quant_filtered.csv")
 
 if __name__ == "__main__":
     main()
